4683eb0a1882ff1b7e490fd321964c00c6b92278
jlofling <justin.lofling@gmail.com>
2017-09-23 16:42:06 -0700

    Test

---
+++ test/test/Effects/FlashEffect.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Launchpad.NET.Effects;
+using Launchpad.NET.Models;
+using Launchpad.NET;
+
+namespace test.Effects
+{
+    public class FlashEffect : ILaunchpadEffect
+    {
+
+        int x = 1;
+        bool isGoingRight = true;
+        LaunchpadButton lastButton;
+
+        public string Name => throw new NotImplementedException();
+
+        public IObservable<ILaunchpadEffect> WhenComplete => null; //throw new NotImplementedException();
+
+        List<LaunchpadButton> gridButtons;
+
+        public void Initiate(List<LaunchpadButton> gridButtons, List<LaunchpadButton> sideButtons, List<LaunchpadTopButton> topButtons, IObservable<ILaunchpadButton> whenButtonStateChanged)
+        {
+            this.gridButtons = gridButtons;
+
+            whenButtonStateChanged
+                .Subscribe(ProcessInput);
+        }
+
+
+        public void ProcessInput(ILaunchpadButton button)
+        {
+            switch (button)
+            {
+                case LaunchpadButton gridButton:
+                    // Clear above
+                    break;
+            }
+        }
+
+        public void Terminate()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update()
+        {
+            if (lastButton != null)
+                lastButton.Color = (byte)LaunchpadMK2Color.Pink;
+
+            if(isGoingRight)
+            {
+                x++;
+            }
+            else
+            {
+                x--;
+            }
+            if(x > 7)
+            {
+                isGoingRight = false;
+            }
+            if(x == 1)
+            {
+                isGoingRight = true;
+            }
+            var nextButton = gridButtons.First(button => button.Id == (byte)(int.Parse("3" + x.ToString())));
+            nextButton.Color = (byte)LaunchpadMK2Color.Off;
+            lastButton = nextButton;
+        }
+    }
+}
---
+++ test/test/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Launchpad.NET;
+using Launchpad.NET.Effects;
+using test.Effects;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace test
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+           this.InitializeComponent();
+            init();
+
+        }
+
+        async void init()
+        {
+            
+            var launchpad = await Novation.Launchpad("launchpad");
+            launchpad.SetButtonColor(3, 5, (byte)LaunchpadMK2Color.Orange);
+            launchpad.RegisterEffect(new FlashEffect(), TimeSpan.FromMilliseconds(100));
+
+        }
+
+    }
+}
